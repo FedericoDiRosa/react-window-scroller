5c36456f1c8f8b82ea2d2700279e12ddcacbe7ba
Federico Di Rosa <federico.dirosa88@gmail.com>
2020-07-05 20:27:38 +0100

    Updates

---
+++ precache-manifest.4facce00762c11333dcd20556613aaaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8378765e427c845babe47966411f0c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "067c0e42621f903c8b09",
+    "url": "./static/css/main.6d4785b2.chunk.css"
+  },
+  {
+    "revision": "7c4fd1e6df56a81bc130",
+    "url": "./static/js/2.17e0decd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.17e0decd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "067c0e42621f903c8b09",
+    "url": "./static/js/main.27d58306.chunk.js"
+  },
+  {
+    "revision": "63825bb38c08a6205555",
+    "url": "./static/js/runtime-main.8132d6ac.js"
+  }
+]);
